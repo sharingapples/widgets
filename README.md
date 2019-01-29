5f30645fcbc0839dd28bf3d2b5c4446ba81276a2
Ranjan Shrestha <ranjan@sharingapples.com>
2019-01-29 14:29:49 +0545

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Native Widgets
+
+## StatusBar
+
+## BottomNavigation
---
+++ index.js
@@ -0,0 +1,8 @@
+// Entry point to import and export all widgets
+import BottomNavigation from './BottomNavigation/BottomNavigation';
+import StatusBar from './StatusBar/StatusBar';
+
+export {
+  BottomNavigation,
+  StatusBar,
+}
