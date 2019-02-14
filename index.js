89947e86d17dc983994af5d7b21b0953d881d718
Ranjan Shrestha <ranjan@sharingapples.com>
2019-02-14 14:04:37 +0545

    Add a Popup that appears from bottom

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // Entry point to import and export all widgets
 import BottomNavigation from './BottomNavigation/BottomNavigation';
 import StatusBar from './StatusBar/StatusBar';
+import BottomPopup from './BottomPopup/BottomPopup';
 
 export {
   BottomNavigation,
   StatusBar,
-}
+  BottomPopup,
+};
