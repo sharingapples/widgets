a5fecdb8b901492a338c59e62291a5fdcd6fafe6
Aashish Karki <aashish.y2z@gmail.com>
2019-04-04 14:11:31 +0545

    refactor

--- Calendar/util.js
+++ Calendar/util.js
@@ -7,21 +7,11 @@ export const SEVEN_DAYS = 7;
 export const SINGLE_DAY = 1;
 
 
-const style = {
-  borderTopColor: 'blue',
-  borderLeftColor: 'blue',
-  borderRightColor: 'blue',
-  borderBottomColor: 'blue',
-};
-
-const borderStyles = Array(16).fill(style).map((n, idx) => {
-  const cellStyle = { ...n };
-  const bitArrays = idx.toString(2).padStart(4, '0').split('');
-  cellStyle.borderTopColor = bitArrays[3] === '1' ? 'transparent' : 'blue';
-  cellStyle.borderRightColor = bitArrays[2] === '1' ? 'transparent' : 'blue';
-  cellStyle.borderBottomColor = bitArrays[1] === '1' ? 'transparent' : 'blue';
-  cellStyle.borderLeftColor = bitArrays[0] === '1' ? 'transparent' : 'blue';
-  return cellStyle;
+const borderStyles = Array(16).fill(null).map((n, idx) => ({
+  borderTopColor: idx & 8 ? 'tranparent' : 'blue',
+  borderRightColor: idx & 4 ? 'transparent': 'blue',
+  borderBottomColor: idx & 2 ? 'transparent' : 'blue',
+  borderLeftColor: idx & 1 ? 'transparent' : 'blue',
 });
 
 export function getDateBorderStyle(date, selectedDates = {}) {
@@ -34,14 +24,10 @@ export function getDateBorderStyle(date, selectedDates = {}) {
   const nextDay = date + SINGLE_DAY * DAY_DIFF;
   const prevDay = date - SINGLE_DAY * DAY_DIFF;
 
-  const isSunday = new Date(date).getDay() === 0;
-  const isSaturday = new Date(date).getDay() === 6;
-
-
   const bit0 = selectedDates[prevWeek] ? 1 : 0;
-  const bit1 = selectedDates[nextDay] && !isSaturday ? 2 : 0;
+  const bit1 = selectedDates[nextDay]  ? 2 : 0;
   const bit2 = selectedDates[nextWeek] ? 4 : 0;
-  const bit3 = selectedDates[prevDay] && !isSunday ? 8 : 0;
+  const bit3 = selectedDates[prevDay] ? 8 : 0;
 
   // eslint-disable-next-line no-bitwise
   const borderStyleIndex = bit0 | bit1 | bit2 | bit3;
