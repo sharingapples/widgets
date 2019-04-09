7c110b6d7a89df266574af7b44949581d06a5bbb
Ranjan Shrestha <ranjan@sharingapples.com>
2019-04-09 15:58:17 +0545

    Setup calendar widget

--- packages/Widgets/index.js
+++ packages/Widgets/index.js
@@ -2,9 +2,11 @@
 import SafePadding from '@sharingapples/safe-padding';
 import StatusBar from '@sharingapples/status-bar';
 import BottomNavigation from '@sharingapples/bottom-navigation';
+import Calendar from '@sharingapples/calendar';
 
 export {
   SafePadding,
   StatusBar,
   BottomNavigation,
+  Calendar,
 };
