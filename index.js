82c7de5f20175bce5ad7740f662901336dc5472e
Ranjan Shrestha <ranjan@sharingapples.com>
2019-04-09 15:31:58 +0545

    Use theme in the showcase app

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-import './showcase';
+import { setTheme } from '@sharingapples/theme';
+import material from '@sharingapples/theme/stock/material';
+
+setTheme(Object.assign(material, { primary: '#ffff00', onPrimary: 'black' }));
+
+require('./showcase');
