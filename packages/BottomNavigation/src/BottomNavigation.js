a5fe9efd3cbbb564be4d9550539865cbfdfafd92
Ranjan Shrestha <ranjan@sharingapples.com>
2019-04-15 14:51:11 +0545

    [BottomNavigation] Add basic documentation

--- packages/BottomNavigation/src/BottomNavigation.js
+++ packages/BottomNavigation/src/BottomNavigation.js
@@ -33,6 +33,28 @@ const styles = StyleSheet.create({
 
 const behavior = Platform.OS === 'ios' ? 'height' : undefined;
 
+/**
+ * Display a navigation bar at the bottom of the screen, with each navigation
+ * item navigating to its repective screen when pressed.
+ *
+ * @param {object} props BottomNavigation props
+ * @param {Class<Component>} props.home The default home screen to display at the beginning
+ * @example <caption>Navigation Example</caption>
+ * import React from 'react';
+ * import BottomNavigation from '@sharingapples/bottom-navigation';
+ * // import { BottomNavigation } from '@sharingapples/widgets';
+ * import { Screen1, Screen2, Screen3 } from './screens';
+ *
+ * export default function App() {
+ *   return (
+ *     <BottomNavigation home={Screen2}>
+ *       <BottomNavigation.Item title="Screen1" screen={Screen1} />
+ *       <BottomNavigation.Item title="Screen2" screen={Screen2} />
+ *       <BottomNavigation.Item title="Screen3" screen={Screen3} />
+ *     </BottomNavigation>
+ *   );
+ * }
+ */
 function BottomNavigation({ home, ...other }: Props) {
   const [Screen, setScreen] = useState(() => home);
 
