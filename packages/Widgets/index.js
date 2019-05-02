30946779b723095855444757eab20386efc0c4e9
Aashish Karki <aashish.y2z@gmail.com>
2019-05-02 17:36:35 +0545

    update package access

--- packages/Widgets/index.js
+++ packages/Widgets/index.js
@@ -3,10 +3,12 @@ import SafePadding from '@sharingapples/safe-padding';
 import StatusBar from '@sharingapples/status-bar';
 import BottomNavigation from '@sharingapples/bottom-navigation';
 import Calendar from '@sharingapples/calendar';
+import RootView from '@sharingapples/rootview';
 
 export {
   SafePadding,
   StatusBar,
   BottomNavigation,
   Calendar,
+  RootView,
 };
